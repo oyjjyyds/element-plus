63c9bb92bed0150a2c3c387cbe664ad60d1e6cdb
oyjjyyds <155054292+oyjjyyds@users.noreply.github.com>
2024-01-31 14:43:04 +0800

    Update html-content.vue

--- docs/examples/tooltip/html-content.vue
+++ docs/examples/tooltip/html-content.vue
@@ -1,8 +1,8 @@
 <template>
   <el-tooltip
-    content="<span>The content can be <strong>HTML</strong></span>"
+    content="<span>The content can be <strong>https://www.hao123.com/</strong></span>"
     raw-content
   >
-    <el-button>hover me</el-button>
+    <el-button>hover</el-button>
   </el-tooltip>
 </template>
